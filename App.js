b61cf4936d9ae3205c7dcf572a5f5b17cac7749a
cutraniramlan <cutraniramlan@gmail.com>
2023-03-19 17:23:36 +0100

    update

--- App.js
+++ App.js
@@ -1,54 +1,29 @@
 import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Tab = createBottomTabNavigator();
 
-// import the screens
+// CREATED COMPONENTS //////////
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 
-// import react Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
 
-// import Firestore
-import { getFirestore } from "firebase/firestore";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-import { LogBox } from "react-native";
-LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
-
-const App = () => {
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyACuO3Ewx4paWZruug4ezh2-suRHBG2NzM",
-    authDomain: "chatapp-63987.firebaseapp.com",
-    projectId: "chatapp-63987",
-    storageBucket: "chatapp-63987.appspot.com",
-    messagingSenderId: "491146204358",
-    appId: "1:491146204358:web:e69c07adf61f8f64a973c7",
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  // Create the navigator
-  const Stack = createNativeStackNavigator();
-
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat">
-          {(props) => <Chat db={db} {...props} />}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-export default App;
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Start">
+          <Tab.Screen name="Start" component={Start} />
+          <Tab.Screen name="Chat" component={Chat} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
--- components/Start.js
+++ components/Start.js
@@ -1,71 +1,199 @@
+import React from "react";
 import {
-  StyleSheet,
   View,
   Text,
-  TouchableOpacity,
-  Alert,
+  Button,
+  TextInput,
+  StyleSheet,
   ImageBackground,
+  TouchableOpacity,
 } from "react-native";
-import { getAuth, signInAnonymously } from "firebase/auth";
 
-const Start = ({ navigation }) => {
-  const auth = getAuth();
+// an object of objects. When referenced in a "style" attribute, the backgroundColor is applied!
+const bgColors = {
+  black: { backgroundColor: "#000000" },
+  gray: { backgroundColor: "#8a95a5" },
+  purple: { backgroundColor: "#474056" },
+  green: { backgroundColor: "#94ae89" },
+};
 
-  const signInUser = () => {
-    signInAnonymously(auth)
-      .then((result) => {
-        navigation.navigate("Chat", { userID: result.user.uid });
-        Alert.alert("Signed in Successfully!");
-      })
-      .catch((error) => {
-        Alert.alert("Unable to sign in, try later again.");
-      });
-  };
+export default class Start extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      bgColor: "",
+    };
+  }
 
-  return (
-    <View style={styles.container}>
+  render() {
+    // black = bgColors.black i.e., { backgroundColor: "#000000" }
+    const { black, gray, purple, green } = bgColors;
+    return (
       <ImageBackground
         source={require("../assets/BackgroundImage2.png")}
-        style={styles.image}
+        style={[styles.container, styles.columnEvenlyCenter]}
       >
-        <Text style={styles.appTitle}></Text>
-        <TouchableOpacity style={styles.startButton} onPress={signInUser}>
-          <Text style={styles.startButtonText}>Get Started</Text>
-        </TouchableOpacity>
+        <Text style={styles.title}>Chat Tack</Text>
+
+        <View style={[styles.nameInput__container, styles.columnEvenlyCenter]}>
+          <TextInput
+            style={styles.nameInput__input}
+            onChangeText={(name) => this.setState({ name })} // state.name is the user's input value
+            value={this.state.name}
+            placeholder="Enter your Name"
+          />
+
+          <View style={styles.colorSelect}>
+            <Text style={styles.colorSelect__text}>
+              Choose your Background:
+            </Text>
+            <View style={styles.colorSelect__dotsWrapper}>
+              <TouchableOpacity
+                style={[
+                  styles.colorSelect__dot,
+                  black,
+                  this.state.color === black.backgroundColor
+                    ? styles.colorSelect__dotSelected
+                    : {},
+                ]}
+                onPress={() =>
+                  this.setState({ bgColor: black.backgroundColor })
+                } //
+              />
+
+              <TouchableOpacity
+                style={[
+                  styles.colorSelect__dot,
+                  gray,
+                  this.state.color === gray.backgroundColor
+                    ? styles.colorSelect__dotSelected
+                    : {},
+                ]}
+                onPress={() => this.setState({ bgColor: gray.backgroundColor })}
+              />
+
+              <TouchableOpacity
+                style={[
+                  styles.colorSelect__dot,
+                  purple,
+                  this.state.color === purple.backgroundColor
+                    ? styles.colorSelect__dotSelected
+                    : {},
+                ]}
+                onPress={() =>
+                  this.setState({ bgColor: purple.backgroundColor })
+                }
+              />
+
+              <TouchableOpacity
+                style={[
+                  styles.colorSelect__dot,
+                  green,
+                  this.state.color === green.backgroundColor
+                    ? styles.colorSelect__dotSelected
+                    : {},
+                ]}
+                onPress={() =>
+                  this.setState({ bgColor: green.backgroundColor })
+                }
+              />
+            </View>
+          </View>
+          <TouchableOpacity
+            style={styles.fauxButton}
+            onPress={() =>
+              this.props.navigation.navigate("Chat", {
+                name: this.state.name || "no-name",
+                bgColor: this.state.bgColor || bgColors.gray.backgroundColor,
+              })
+            }
+          >
+            <Text style={[styles.colorSelect__text, styles.fauxButton__text]}>
+              Start Chatting
+            </Text>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
-  appTitle: {
-    fontWeight: "600",
-    fontSize: 45,
-    marginBottom: 100,
-  },
-  startButton: {
-    backgroundColor: "#000",
-    height: 50,
-    width: "88%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  startButtonText: {
-    color: "#FFF",
-  },
-  image: {
-    flex: 1,
+
+  columnEvenlyCenter: {
+    display: "flex",
     flexDirection: "column",
     justifyContent: "space-evenly",
     alignItems: "center",
-    resizeMode: "cover",
-    width: "100%",
+  },
+
+  title: {
+    color: "#fff",
+    fontSize: 45,
+    fontWeight: "600",
+  },
+
+  nameInput__container: {
+    backgroundColor: "#fff",
+    height: "44%",
+    minHeight: 200,
+    width: "88%",
+  },
+
+  nameInput__input: {
+    height: 50,
+    width: "88%",
+    paddingLeft: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+    color: "#757083",
+    opacity: 50,
+    fontSize: 16,
+    fontWeight: "300",
+  },
+
+  colorSelect: {
+    height: 75,
+  },
+
+  colorSelect__text: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 100,
+  },
+
+  colorSelect__dotsWrapper: {
+    flexDirection: "row",
+  },
+
+  colorSelect__dot: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    margin: 10,
+  },
+
+  colorSelect__dotSelected: {
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "#5f5f5f",
+  },
+
+  fauxButton: {
+    backgroundColor: "#757083",
+    justifyContent: "center",
+    width: "88%",
+    padding: 16,
+  },
+
+  fauxButton__text: {
+    color: "#fff",
+    fontWeight: "600",
   },
 });
-
-export default Start;
